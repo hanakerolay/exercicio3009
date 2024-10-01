efd1f37af8aad231f330131f8551e4f6526a8949
Kerolay <hana.kerolay@gmail.com>
2024-09-30 23:52:53 -0300

    subirconclusao

--- js/script.js
+++ js/script.js
@@ -31,7 +31,83 @@ function adicionarValor() {
 }
 
 
+function calcularSaldo() {
+    let saldo = Number(document.getElementById('saldo').value);
+
+    if (isNaN(saldo) || saldo < 0) {
+        document.getElementById('resultado').innerText = "Por favor, insira um saldo inicial válido.";
+        return;
+    }
+
+    for (let i = 1; i <= 5; i++) {
+        const tipoOperacao = document.getElementById('tipo' + i).value;
+        const valorOperacao = parseFloat(document.getElementById('valor' + i).value);
+
+        if (isNaN(valorOperacao) || valorOperacao < 0) {
+            document.getElementById('resultado').innerText = `Por favor, insira um valor válido para a operação ${i}.`;
+            return;
+        }
+
+        if (tipoOperacao === 'credito') {
+            saldo += valorOperacao;
+        } else if (tipoOperacao === 'debito') {
+            saldo -= valorOperacao;
+        }
+    }
+
+    document.getElementById('resultadoSaldo').innerText = 'Saldo Final: R$' + saldo.toFixed(2);
+}
 
 
 
-        
+
+
+
+
+
+
+function gerarRegistros() {
+    let totalAulas = Number(document.getElementById('totalAulas').value);
+    let registroPresencas = document.getElementById('registroPresencas');
+
+    if (isNaN(totalAulas) || totalAulas <= 0) {
+        document.getElementById('resultadoPresen').innerText = "Por favor, insira um número válido de aulas.";
+        return;
+    }
+
+    registroPresencas.innerHTML = '';
+    document.getElementById('resultadoPresen').innerText = '';
+
+    for (let i = 1; i <= totalAulas; i++) {
+        registroPresencas.innerHTML += `
+            <div class="aula">
+                <label for="aula${i}">Aula ${i}:</label>
+                <select id="aula${i}">
+                    <option value="presente">Presente</option>
+                    <option value="ausente">Ausente</option>
+                </select>
+            </div>
+        `;
+    }
+
+    document.getElementById('calcularPresen').style.display = 'inline-block';
+}
+
+function calcularResultado() {
+    let totalAulas = Number(document.getElementById('totalAulas').value);
+    let totalPresencas = 0;
+
+    for (let i = 1; i <= totalAulas; i++) {
+        const statusAula = document.getElementById(`aula${i}`).value;
+        if (statusAula === 'presente') {
+            totalPresencas++;
+        }
+    }
+
+    const percentualPresenca = (totalPresencas / totalAulas) * 100;
+    const resultadoPresen = percentualPresenca >= 75
+        ? `Aprovado com ${percentualPresenca.toFixed(2)}% de presença`
+        : `Reprovado com ${percentualPresenca.toFixed(2)}% de presença`;
+
+    document.getElementById('resultadoPresen').innerText = resultadoPresen;
+}
